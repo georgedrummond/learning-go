6846f2f86d41248b1d95ad1a98f77b2095a2aa34
George Drummond <georgedrummond@gmail.com>
2014-08-27 20:47:44 +0100

    printing strings

---
+++ example.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Person struct {
+	name string
+	age  int
+}
+
+func (p *Person) Welcome() string {
+	return fmt.Sprintf("Welcome %v! You are %v.", p.name, p.age)
+}
+
+func main() {
+	me := Person{name: "George", age: 26}
+
+	fmt.Println(me.Welcome())
+}
