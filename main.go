dd5981407a900bd331432956db834a12fc8f87a6
George Drummond <georgedrummond@gmail.com>
2014-08-28 10:01:51 +0100

    Playing with builds in Go

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"./something"
+	"fmt"
 )
 
 type Person struct {
---
+++ something/something_test.go
@@ -0,0 +1,11 @@
+package something
+
+import "testing"
+
+func TestAnything(t *testing.T) {
+  t.Error("no")
+}
+
+func TestB(t *testing.T) {
+  t.Error("no")
+}
