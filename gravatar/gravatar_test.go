6dcafd9d1f95ef00750a4671bbb8217a262ad395
George Drummond <georgedrummond@gmail.com>
2014-08-31 00:31:06 +0100

    Remove old test

--- gravatar/gravatar_test.go
+++ gravatar/gravatar_test.go
@@ -9,8 +9,6 @@ func TestUrl(t *testing.T) {
 	expected := "https://gravatar.com/avatar/d278a12b969a495ab16fdd942e748fe5"
 	profile := Profile{Email: "georgedrummond@gmail.com"}
 
-	assert.Equal(t, profile.Size, 0)
-
 	assert.Equal(t, profile.Url(), expected)
 }
 
