95c6db3e6b7de10d92fb470b18d1af0437b0345d
George Drummond <georgedrummond@gmail.com>
2014-08-30 23:01:55 +0100

    Use assert

--- gravatar/gravatar_test.go
+++ gravatar/gravatar_test.go
@@ -1,6 +1,7 @@
 package gravatar
 
 import (
+	"github.com/bmizerany/assert"
 	"testing"
 )
 
@@ -8,7 +9,5 @@ func TestEmailUrl(t *testing.T) {
 	gravatar_url := EmailUrl("georgedrummond@gmail.com")
 	expected := "https://gravatar.com/avatar/d278a12b969a495ab16fdd942e748fe5"
 
-	if gravatar_url != expected {
-		t.Fatalf("Incorrect gravatar url formed (actual=%s, expected=%s)", gravatar_url, expected)
-	}
+	assert.Equal(t, gravatar_url, expected)
 }
